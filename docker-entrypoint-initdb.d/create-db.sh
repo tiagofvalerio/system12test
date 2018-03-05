ab46ee207c8c8ce7cc59f0b09fee6f8e63218329
tvalerio <tiidle@gmail.com>
2018-03-05 11:45:08 -0300

    Correção docker-compose e script.

--- docker-entrypoint-initdb.d/create-db.sh
+++ docker-entrypoint-initdb.d/create-db.sh
@@ -6,4 +6,24 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE system2;
     GRANT ALL PRIVILEGES ON DATABASE system1 TO postgres;
     GRANT ALL PRIVILEGES ON DATABASE system2 TO postgres;
+
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --dbname "system1" --username "$POSTGRES_USER" <<-EOSQL
+    CREATE TABLE test
+    (
+      uuid        UUID                        NOT NULL,
+      description CHARACTER VARYING(100)      NULL,
+      CONSTRAINT test_pkey PRIMARY KEY (uuid)
+    );
+
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --dbname "system2" --username "$POSTGRES_USER" <<-EOSQL
+    CREATE TABLE test
+    (
+      uuid        UUID                        NOT NULL,
+      description CHARACTER VARYING(100)      NULL,
+      CONSTRAINT test_pkey PRIMARY KEY (uuid)
+    );
 EOSQL
